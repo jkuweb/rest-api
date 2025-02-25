ecf6c5de2619d5e0c06ff2ee6a010e36f1cd5793
Joseba kerejeta <josebaquerejetaweb@gmail.com>
2025-02-25 19:01:33 +0100

    ejercicio2

--- src/pods/character-collection/api/character-collection.api.ts
+++ src/pods/character-collection/api/character-collection.api.ts
@@ -1,4 +1,6 @@
 import { ENV_VARIABLES } from "#core/env";
+import { CharacterApiModel } from "#pods/character/api";
+import axios from "axios";
 import { CharacterMemberApiModel } from "./character-collection.api.model";
 
 // export const getCharacterCollectionPerPage = async (page: string) => {
@@ -9,11 +11,18 @@ import { CharacterMemberApiModel } from "./character-collection.api.model";
 
 //   return response.results;
 // };
+// export const getCharacterCollectionApi = async (): Promise<CharacterMemberApiModel[]> => {
+//   const response = await fetch(
+//     `${ENV_VARIABLES.CHARACTERS_API_BASE_URL}/character`
+//   ).then(response => response.json());
+
+
+//   return response.results;
+// };
+
+const url = '/api/character';
+
 export const getCharacterCollectionApi = async (): Promise<CharacterMemberApiModel[]> => {
-  const response = await fetch(
-    `${ENV_VARIABLES.CHARACTERS_API_BASE_URL}/character`
-  ).then(response => response.json());
-
-
-  return response.results;
+  const { data } = await axios.get<CharacterApiModel[]>(url);
+  return data;
 };
